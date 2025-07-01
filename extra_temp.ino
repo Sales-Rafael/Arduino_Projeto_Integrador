67b6d90ea5278c3ff5a18cc10ae1765a1eaff3fe
Rafael Sales <153143536+Sales-Rafael@users.noreply.github.com>
2025-07-01 15:04:59 -0300

    Update extra_temp.ino

--- extra_temp.ino
+++ extra_temp.ino
@@ -2,12 +2,16 @@
 
 #define DHTPIN 2        // Pino conectado ao DHT11
 #define DHTTYPE DHT11   // Tipo do sensor
+#define LedVermelho 11
+#define LedAzul 12
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  pinMode(12, OUTPUT); 
+  pinMode(11, OUTPUT); 
 }
 
 void loop() {
@@ -22,4 +26,11 @@ void loop() {
   Serial.print(" % | Temperatura: ");
   Serial.print(temperatura);
   Serial.println(" °C");
+  if (temperatura >= 26.5) {
+    digitalWrite(LedAzul, HIGH);
+    digitalWrite(LedVermelho, LOW);
+  } else {
+    digitalWrite(LedAzul, LOW);
+    digitalWrite(LedVermelho, HIGH);
+  }
 }
